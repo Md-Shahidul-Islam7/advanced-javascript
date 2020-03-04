6f4ec04daef7f78f5df64e1cce1b8559ac6bcdec
Md. Shahidul Islam <mdsislam.info@gmail.com>
2020-03-04 09:14:14 +0600

    Learned null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,26 @@
+let bird; - undefined
+console.log(bird);
+
+function add(num1, num2) {
+  console.log(num1 + num2);
+  return
+} 
+const result = add(13, 82);
+console.log(result);
+
+function add(num1, num2) {
+  console.log(num1, num2);
+}
+const result = add(13);
+console.log(result);
+
+const lover = { name: "smart dude", phone: 4545454 };
+console.log(lover.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[5]);  undefined is negative or falsy
+
+// null not existence
